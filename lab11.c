590661a05310b091a81fb70d9b6dbcdb59273875
Arun Karki <arunkarki3629@gmail.com>
2023-11-23 12:43:05 -0500

    cleanup

--- lab11.c
+++ lab11.c
@@ -15,8 +15,7 @@
 #define MAX_QUEUE_PROCESS 100 // max of 100 processes per queue
 
 // basic process information
-struct process_information
-{
+struct process_information{
     int PID;                        // pid generated
     int STATIC_PRIORITY;            // default = 120; shud be b/w 100-139, lower # = higher priority
     int DYNAMIC_PRIORITY;           // changing priority value
@@ -30,8 +29,7 @@ struct process_information
 };
 
 // ready queue structure
-struct core_queues
-{
+struct core_queues{
     struct process_information RQ_0[MAX_QUEUE_PROCESS]; // ready queue 0
     struct process_information RQ_1[MAX_QUEUE_PROCESS]; // ready queue 1
     struct process_information RQ_2[MAX_QUEUE_PROCESS]; // ready queue 2
@@ -40,7 +38,7 @@ struct core_queues
 // globals
 pthread_mutex_t mutex_lock;            // lock to do CS
 struct core_queues cpu_cores[NUM_CPU]; // create N struct queues; each represents 1 core
-int running_processes = 0;                 // TESTING: use this to stop the program once 0 processes are running
+int ready_processes = 0;                 // TESTING: use this to stop the program once 0 processes are running
 
 
 // thread functions
@@ -48,44 +46,23 @@ void *producer_thread_function();
 void *cpu_thread_function(void *arg);
 
 // generate random numbers between [min, max] range
-int generate_int(int min, int max)
-{
-    if (min >= max)
-    {
-        min = max;
-    }
+int generate_int(int min, int max){
+    if (min >= max) {min = max;}
     return (rand() % (max - min + 1)) + min;
 }
 
 // pick a ready queue depending on the priority
-int pick_ready_queue(int priority)
-{
-    if (priority >= 0 && priority < 100)
-    {
-        return 0; // RQ_0
-    }
-    else if (priority >= 100 && priority < 130)
-    {
-        return 1; // RQ_1
-    }
-    else if (priority >= 130 && priority < 140)
-    {
-        return 2; // RQ_2
-    }
-    else
-    {
-        perror("invalid priority");
-        exit(EXIT_FAILURE);
-    }
+int pick_ready_queue(int priority){
+    if(priority >= 0 && priority < 100) {return 0;} // RQ_0
+    else if(priority >= 100 && priority < 130) {return 1;} // RQ_1
+    else if (priority >= 130 && priority < 140) {return 2;} // RQ_2
+    else {perror("invalid priority"); exit(EXIT_FAILURE);}
 }
 
 // search the ready queue for an empty index and return it
-int find_index(struct process_information *queue)
-{
-    for (int i = 0; i < MAX_QUEUE_PROCESS; i++)
-    {
-        if (queue->PID == 0)
-        {
+int find_index(struct process_information *queue){
+    for(int i = 0; i < MAX_QUEUE_PROCESS; i++){
+        if(queue->PID == 0){
             return i;
         }
     }
@@ -93,20 +70,16 @@ int find_index(struct process_information *queue)
 }
 
 // find the first index of a ready process
-int find_ready_index(struct process_information *queue)
-{
-    for (int i = 0; i < MAX_QUEUE_PROCESS; i++)
-    {
-        if (queue->PID > 0)
-        {
+int find_ready_index(struct process_information *queue){
+    for(int i = 0; i < MAX_QUEUE_PROCESS; i++){
+        if(queue->PID > 0){
             return i;
         }
     }
     return -1;
 }
 
-int main()
-{
+int main(){
     /* one thread = producer, multi-threads = consumers */
     srand(time(NULL));
 
@@ -115,24 +88,15 @@ int main()
     pthread_t cpu_threads[NUM_CPU]; // array of consumer threads (cpus)
 
     // init mutex lock
-    if (pthread_mutex_init(&mutex_lock, NULL) != 0)
-    {
-        perror("Mutex initialization failed");
-        exit(EXIT_FAILURE);
-    }
+    if(pthread_mutex_init(&mutex_lock, NULL) != 0) {perror("Mutex initialization failed"); exit(EXIT_FAILURE);}
 
     // create producer thread
-    if (pthread_create(&producer_thread, NULL, producer_thread_function, NULL) != 0)
-    {
-        perror("Thread creation failed");
-        exit(EXIT_FAILURE);
-    }
+    if(pthread_create(&producer_thread, NULL, producer_thread_function, NULL) != 0){perror("Thread creation failed"); exit(EXIT_FAILURE);}
 
     usleep(100); // give producer a chance to produce before consuming for loop
 
     // create consumer threads
-    for (int thread_index = 0; thread_index < NUM_CPU; thread_index++)
-    {
+    for(int thread_index = 0; thread_index < NUM_CPU; thread_index++){
 
         // cons attr
         pthread_t c_thread = cpu_threads[thread_index];
@@ -140,19 +104,12 @@ int main()
         void *consumer_args = thread_index;
 
         // set consumer thread to be detached
-        if (pthread_attr_init(&c_thread_attr) != 0)
-        {
-            perror("Attribute creation failed");
-            exit(EXIT_FAILURE);
-        }
+        if(pthread_attr_init(&c_thread_attr) != 0) {perror("Attribute creation failed"); exit(EXIT_FAILURE);}
+
         pthread_attr_setdetachstate(&c_thread_attr, PTHREAD_CREATE_DETACHED); // set it in a detached state; dont wait for anything
 
         // create consumer thread
-        if (pthread_create(&c_thread, &c_thread_attr, cpu_thread_function, &consumer_args) != 0)
-        {
-            perror("Thread creation failed");
-            exit(EXIT_FAILURE);
-        }
+        if(pthread_create(&c_thread, &c_thread_attr, cpu_thread_function, &consumer_args) != 0){perror("Thread creation failed"); exit(EXIT_FAILURE);}
 
         // free attribute resources
         (void)pthread_attr_destroy(&c_thread_attr);
@@ -162,11 +119,7 @@ int main()
     }
 
     // wait for producer to return; idk if necessary atm
-    if (pthread_join(producer_thread, &thread_return) != 0)
-    {
-        perror("Thread join failed");
-        exit(EXIT_FAILURE);
-    }
+    if(pthread_join(producer_thread, &thread_return) != 0) {perror("Thread join failed"); exit(EXIT_FAILURE);}
 
     // just end after 2 secs
     sleep(2);
@@ -178,33 +131,25 @@ int main()
 }
 
 // producer thread
-void *producer_thread_function()
-{
+void *producer_thread_function(){
 
     printf("[PRODUCER_ID_%lu]: thread function invoked\n", pthread_self());
     // printf("[PRODUCER_ID_%lu]: generating %d PCB...\n", pthread_self(), NUM_PCB_GENERATE);
 
     // opening csv file
     FILE *file = fopen("pcb_data.csv", "r");
-    if (file == NULL)
-    {
-        perror("Error opening file");
-        return 1;
-    }
+    if(file == NULL) {perror("Error opening file"); return 1;}
 
     // store data read from csv file in here
     struct ProcessData process_data_copy;
 
-    while (1)
-    {
+    while(1){
 
         int result = parse_csv_line(file, &process_data_copy);
-        if (result == 1)
-        {
+        if(result == 1){
 
             // if i can put in any cpu then pick one at random
-            if (process_data_copy.cpu_affinity == -1)
-            {
+            if(process_data_copy.cpu_affinity == -1){
                 process_data_copy.cpu_affinity = generate_int(0, 3);
             }
 
@@ -216,18 +161,13 @@ void *producer_thread_function()
             struct process_information *pcb;
             int index; // index in queue
 
-            if (ready_queue == 0)
-            { // RO
+            if(ready_queue == 0){               // RO
                 index = find_index(&(core->RQ_0));
                 pcb = &(core->RQ_0)[index];
-            }
-            else if (ready_queue == 1)
-            { // R1
+            }else if(ready_queue == 1){         // R1
                 index = find_index(&(core->RQ_1));
                 pcb = &(core->RQ_1)[index];
-            }
-            else
-            { // R2
+            }else{                              // R2
                 index = find_index(&(core->RQ_2));
                 pcb = &(core->RQ_2)[index];
             }
@@ -244,15 +184,13 @@ void *producer_thread_function()
             pcb->CPU_AFFINITY = process_data_copy.cpu_affinity;
             pcb->SCHED_POLICY = process_data_copy.policy;
 
-            running_processes++;
+            ready_processes++;
 
             pthread_mutex_unlock(&mutex_lock);
 
             // "To demonstrate the effect of the producer/consumer solution..." -> add a little sleep
             usleep(generate_int(100, 600)); // sleep 100us-600us per each read
-        }
-        else
-        {
+        }else{
             break;
         }
     }
@@ -263,16 +201,14 @@ void *producer_thread_function()
 }
 
 // consumer threat
-void *cpu_thread_function(void *arg)
-{
+void *cpu_thread_function(void *arg){
+
     int thread_index = *(int *)arg;
     printf("[CONSUMER_ID_%lu]: thread #%d invoked\n", pthread_self(), thread_index);
 
     struct core_queues *core = &cpu_cores[thread_index];
 
-    // while # running processes > 0
-    while (running_processes > 0)
-    {
+    while(ready_processes > 0){
 
         // check all queues, in highest priority -> lowest
         struct process_information *pcb;
@@ -281,20 +217,13 @@ void *cpu_thread_function(void *arg)
         int rq2_index = find_ready_index(core->RQ_2);
 
         // find a process to run, if not found, continue loop
-        if (rq0_index > -1)
-        {
+        if(rq0_index > -1){
             pcb = &(core->RQ_0)[rq0_index];
-        }
-        else if (rq1_index > -1)
-        {
+        }else if (rq1_index > -1){
             pcb = &(core->RQ_1)[rq1_index];
-        }
-        else if (rq2_index > -1)
-        {
+        }else if (rq2_index > -1){
             pcb = &(core->RQ_2)[rq2_index];
-        }
-        else
-        {
+        }else{
             continue;
         }
 
@@ -304,12 +233,9 @@ void *cpu_thread_function(void *arg)
         int time_quantum; // in millseconds
 
         // calculate time quantum
-        if (pcb->STATIC_PRIORITY < 120)
-        {
+        if(pcb->STATIC_PRIORITY < 120){
             time_quantum = (140 - pcb->STATIC_PRIORITY) * 20;
-        }
-        else
-        {
+        }else{
             time_quantum = (140 - pcb->STATIC_PRIORITY) * 5;
         }
 
@@ -317,12 +243,9 @@ void *cpu_thread_function(void *arg)
         int bonus = (pcb->EXECUTION_TIME < pcb->TIME_SLICE) ? 10 : 5; //= generate_int(0, 10);
         int DP = pcb->STATIC_PRIORITY - bonus + 5;
 
-        if (DP < 100)
-        {
+        if(DP < 100){
             DP = 100;
-        }
-        else if (DP > 139)
-        {
+        }else if(DP > 139){
             DP = 139;
         }
 
@@ -347,10 +270,9 @@ void *cpu_thread_function(void *arg)
         printf("\tSCHED_POLICY = %d\n}\n", pcb->SCHED_POLICY);
 
         // if 0 time is remaining then process has ended
-        if (pcb->REMAIN_TIME == 0)
-        {
+        if(pcb->REMAIN_TIME == 0){
             pcb->PID = 0;
-            running_processes--;
+            ready_processes--;
         }
 
         pthread_mutex_unlock(&mutex_lock);
